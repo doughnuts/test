c7a976d6b263bd90f89a582a6f62fe7a77f960e1
doughnuts <3400760092@qq.com>
2025-01-29 20:31:18 +0800

    提交了14-os模块.py

---
+++ 14-os模块.py
@@ -0,0 +1,12 @@
+import os
+
+# os.mkdir("./a.txt")
+# print("创建成功")
+# if os.path.exists("a.txt"):
+#     if os.path.isdir("a.txt"):
+#         os.rmdir("a.txt")
+#         print("删除成功")
+
+
+r = os.listdir("./")
+print(r)
